60b0e1e6396463534c4b70c7d360d611d4cee9c8
MihajaPHIDIA <mihaja@phidia.onmicrosoft.com>
2024-08-10 00:33:04 +0300

    sale price alert

---
+++ mfb_restrict_purchase_validation/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'mfb_restrict_purchase_validation',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Mada Fiara Be Restrict Purchase Validation',
+    'description': 'Mada Fiara Be Restrict Purchase Validation',
+    'author': 'Mihaja',
+    'website': 'http://www.example.com',
+    'depends': ['base', 'purchase'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/mfb_sale_order.xml',
+        'view/purchase.xml'
+    ],
+    'installable': True,
+    'application': True,
+}
