19edeb1a2590a92572bffa5a8f6cbb5d1091f1e2
MihajaPHIDIA <mihaja@phidia.onmicrosoft.com>
2024-08-09 23:52:19 +0300

    sale price alert

--- mfb_sale_price_alert/models/models.py
+++ mfb_sale_price_alert/models/models.py
@@ -11,13 +11,19 @@ class SaleOrderLine(models.Model):
     @api.onchange('price_unit')
     def _onchange_price_unit_warning(self):
         if self.price_unit < self.product_id.list_price :
-            self.is_price_modified = True
+            self.write({
+                'is_price_modified': True
+            })
             return {
                 'warning': {
-                'title': _("Warning for %s", self.product_id.name),
+                'title': _("Attention pour %s", self.product_id.name),
                 'message': "Le montant saisissé est inférieur au prix de l'article.",
                 }
             }
+        else :
+            self.write({
+                'is_price_modified': False
+            })
     
     def _action_sale_price_alert(self) :
         print("hello world")
