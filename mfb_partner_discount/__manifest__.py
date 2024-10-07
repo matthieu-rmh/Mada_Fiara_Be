8efa0a06eb942b4e839e6c513e7c762eea774f26
MihajaPHIDIA <mihaja@phidia.onmicrosoft.com>
2024-10-07 21:07:16 +0300

    partner discount

---
+++ mfb_partner_discount/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'mfb_partner_discount',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Mada Fiara Be Partner Discount',
+    'description': 'Mada Fiara Be Partner Discount',
+    'author': 'Mihaja',
+    'website': 'http://www.example.com',
+    'depends': ['base', 'sale'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/mfb_sale_order.xml',
+        'cron/partner.xml'
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ mfb_partner_discount/models/sale.py
@@ -0,0 +1,16 @@
+from odoo import api, fields, models
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.onchange('partner_id')
+    def _onchange_partner_id(self):
+        """
+        Automatically applies the discount from the customer when the partner is selected.
+        """
+        super(SaleOrder, self)._onchange_partner_id()
+        if self.partner_id and self.partner_id.customer_discount:
+            discount = self.partner_id.customer_discount
+            for line in self.order_line:
+                line.discount = discount
+
