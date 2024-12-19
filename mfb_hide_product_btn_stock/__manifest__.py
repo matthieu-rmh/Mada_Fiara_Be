67af23aa631ff5ec2a594f43118afc78c634c844
MihajaPHIDIA <mihaja@phidia.onmicrosoft.com>
2024-12-19 16:34:20 +0300

    hide product stock btn

---
+++ mfb_hide_product_btn_stock/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'mfb_hide_product_btn_stock',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Mada Fiara Be Hide product Btn stock',
+    'description': 'Mada Fiara Be Hide product Btn stock',
+    'author': 'Mihaja',
+    'website': 'http://www.example.com',
+    'depends': ['base', 'sale'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/mfb_sale_order.xml',
+        'views/product.xml'
+    ],
+    'installable': True,
+    'application': True,
+}
