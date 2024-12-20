b2e9a2dda6e0e6c712b9d8739e8eaf3fe5fda10c
MihajaPHIDIA <mihaja@phidia.onmicrosoft.com>
2024-12-20 16:20:41 +0300

    show product categ

---
+++ mfb_show_product_categ/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'mfb_show_product_categ',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Mada Fiara Be Show product category',
+    'description': 'Mada Fiara Be Show product category',
+    'author': 'Mihaja',
+    'website': 'http://www.example.com',
+    'depends': ['base', 'purchase', 'stock'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/purchase.xml',
+        # 'cron/sale_price_alert.xml'
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ mfb_show_product_categ/models/purchase.py
@@ -0,0 +1,15 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+from datetime import datetime, timedelta
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    categ_value = fields.Char(string="Catégorie de produit", compute='_compute_categ_value')
+
+    @api.depends('product_id')
+    def _compute_categ_value(self):
+        for rec in self:
+            rec.categ_value = rec.product_id.categ_id.name
+
+    
