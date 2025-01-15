80edb9da4859289adedd865943196422cb91e73b
MihajaPHIDIA <mihaja@phidia.onmicrosoft.com>
2025-01-15 14:43:01 +0300

    dashboard

--- mfb_owl_dashboard/models/users.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError
-from datetime import date
-
-class ResUsers(models.Model):
-	_inherit = 'res.users'
-
-	customer_amount_target = fields.Float(string="Montant objectif")
